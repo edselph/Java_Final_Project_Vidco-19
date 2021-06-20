aa044a3656faef7d63dc4e3ebfe9af3e3285dd15
Sleepy <71368659+edselph@users.noreply.github.com>
2021-06-20 21:55:30 +0700

    Delete FINAL PROJECT directory

--- FINAL PROJECT/fileText.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-public abstract class fileText { // Parent class for read and write
-  ArrayList<userInput> users = new ArrayList<>();// array list for the user
-
-  public abstract void printMessage();
-}
