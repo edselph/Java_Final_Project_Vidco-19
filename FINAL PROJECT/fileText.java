9ae19c72bb23915978151718dd370831d328e9eb
Sleepy <71368659+edselph@users.noreply.github.com>
2021-06-20 21:55:02 +0700

    Add files via upload

---
+++ FINAL PROJECT/fileText.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+public abstract class fileText { // Parent class for read and write
+  ArrayList<userInput> users = new ArrayList<>();// array list for the user
+
+  public abstract void printMessage();
+}
